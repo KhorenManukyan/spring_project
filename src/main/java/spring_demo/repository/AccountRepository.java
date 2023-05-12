98138087c4c6240ef6840a3ad157c844142e13b8
KhorenManukyan <xoren24@gmail.com>
2023-05-12 23:19:38 +0400

    empty branch for new projects

--- src/main/java/spring_demo/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package spring_demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import spring_demo.model.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-
-}
--- src/main/java/spring_demo/service/AccountService.java
+++
@@ -1,5 +0,0 @@
-package spring_demo.service;
-
-public interface AccountService {
-    void transfer(int accountId, int otherAccountId, double balance);
-}
